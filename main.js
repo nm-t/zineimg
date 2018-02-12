f178e9503ffc0ff3a6ed2e5bd6c4e18d8ee08ad5
nmt <nathaliamtan@gmail.com>
2018-02-12 18:18:15 +1100

    Fixed thumbnail issue

--- main.js
+++ main.js
@@ -1,8 +1,7 @@
 const auth = config.AUTH;
-const postmanToken = config.POSTMAN_TOKEN;
 
 function previewFile() {
-  var preview = document.querySelector('img');
+  var preview = document.querySelector('.thumb');
   var file    = document.querySelector('input[type=file]').files[0];
   var reader  = new FileReader();
 
@@ -17,14 +16,16 @@ function previewFile() {
 
 let resp;
 let form = new FormData();
+console.log(form);
 
 function uploadImage() {
   $("#upload-butt").toggle();
   $("#loader").toggle();
 
-  let $img = $('img');
+  let $img = $('.thumb');
   localStorage.imageBase64 = $img.attr('src').replace(/.*,/, '');
   form.append("image", localStorage.imageBase64);
+  form.append("album", "WqXoc");
 
   $.ajax(settings).done(function (response) {
     console.log(response);
@@ -41,9 +42,7 @@ const settings = {
   "url": "https://api.imgur.com/3/image",
   "method": "POST",
   "headers": {
-    "authorization": auth,
-    // "cache-control": "no-cache",
-    // "postman-token": postmanToken
+    "authorization": auth
   },
   "processData": false,
   "contentType": false,
